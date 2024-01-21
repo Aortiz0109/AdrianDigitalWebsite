58b4cede774bd78ad1bf89ff431beb6f027ed4b8
Aortiz0109 <aortiz0109@gmail.com>
2024-01-21 17:22:33 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,113 @@
+$(document).ready(function(){
+
+
+    //currentyear
+    var currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").textContent = currentYear;
+    
+
+    //mobile menu toggling
+    $("#menu_icon").click(function(){
+        $("header nav ul").toggleClass("show_menu");
+        $("#menu_icon").toggleClass("close_menu");
+        return false;
+    });
+
+    //Contact Page Map Centering
+    var hw = $('header').width() + 50;
+    var mw = $('#map').width();
+    var wh = $(window).height();
+    var ww = $(window).width();
+
+    $('#map').css({
+        "max-width" : mw,
+        "height" : wh
+    });
+
+    if(ww>1100){
+         $('#map').css({
+            "margin-left" : hw
+        });
+    }
+
+    //Hidden Button until scroll
+    const showOnPx = 100;
+    const offset = 0;
+    const backToTopButton = document.querySelector(".back-up");
+    const pageProgressBar = document.querySelector(".progress-bar");
+
+    const scrollContainer = () => {
+        return document.documentElement || document.body;
+    };
+
+    const goToTop = () => {
+        document.body.scrollIntoView({
+            behavior: "smooth"
+        });
+    };
+    
+    document.addEventListener("scroll", () => {
+        console.log("Scroll Height: ", scrollContainer().scrollHeight);
+        console.log("Client Height: ", scrollContainer().clientHeight);
+      
+        const scrolledPercentage =
+          ((scrollContainer().scrollTop - offset ) /
+            (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+          100;
+      
+        pageProgressBar.style.width = `${scrolledPercentage}%`;
+      
+        if (scrollContainer().scrollTop > showOnPx) {
+          backToTopButton.classList.remove("btn-hide");
+        } else {
+          backToTopButton.classList.add("btn-hide");
+        }
+      });
+      
+      backToTopButton.addEventListener("click", goToTop);
+
+    //Tooltip
+    $("a").mouseover(function(){
+
+        var attr_title = $(this).attr("data-title");
+
+        if( attr_title == undefined || attr_title == "") return false;
+
+        $(this).after('<span class="tooltip"></span>');
+
+        var tooltip = $(".tooltip");
+        tooltip.append($(this).data('title'));
+
+
+        var tipwidth = tooltip.outerWidth();
+        var a_width = $(this).width();
+        var a_hegiht = $(this).height() + 3 + 4;
+
+        //if the tooltip width is smaller than the a/link/parent width
+        if(tipwidth < a_width){
+            tipwidth = a_width;
+            $('.tooltip').outerWidth(tipwidth);
+        }
+
+        var tipwidth = '-' + (tipwidth - a_width)/2;
+        $('.tooltip').css({
+            'left' : tipwidth + 'px',
+            'bottom' : a_hegiht + 'px'
+        }).stop().animate({
+            opacity : 1
+        }, 200);
+
+
+    });
+
+    $("a").mouseout(function(){
+        var tooltip = $(".tooltip");
+        tooltip.remove();
+    });
+
+
+});
+
+//Image Gallery//
+
+//End Image Gallery//
